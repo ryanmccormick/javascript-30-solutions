92c8f4a3ea717e983b20d54bcdd15140c11138b3
Ryan McCormick <ryan.mccormick@rtmccormick.com>
2024-02-26 19:36:47 -0600

    setup files

---
+++ 03-playing-with-css-variables-and-js/scripts/playing-with-css-vars.js
@@ -0,0 +1,11 @@
+(function(global, docRef) {
+    'use strict';
+
+    function init() {
+        console.log('loaded', performance.now());
+    }
+
+    init();
+    ////////
+
+})(window, document)
