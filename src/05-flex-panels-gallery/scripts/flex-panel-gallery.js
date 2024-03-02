ee07403da60a0383b67731848c29aefe38604d3b
Ryan McCormick <ryan.mccormick@rtmccormick.com>
2024-03-01 22:37:40 -0600

    add gallery implementation

--- src/05-flex-panels-gallery/scripts/flex-panel-gallery.js
+++ src/05-flex-panels-gallery/scripts/flex-panel-gallery.js
@@ -2,9 +2,25 @@
   'use strict';
 
   function init() {
-    console.log('hello world!!');
+    initGallery();
   }
 
   init();
   /////////
+
+  function initGallery() {
+    const panels = docRef.querySelectorAll('.panel');
+    panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+    panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive));
+  }
+
+  function toggleOpen() {
+    this.classList.toggle('open');
+  }
+
+  function toggleActive(event) {
+    if (event.propertyName.includes('flex')) {
+      this.classList.toggle('open-active');
+    }
+  }
 })(window, document);
